ece841a964d31b987dea7a3327a8dbea439996ef
chrisIyer <hellohotbox420@gmail.com>
2023-01-10 14:27:16 -0500

    fixed

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -5,17 +5,20 @@ import { getUser } from '../../utilities/users-service';
 import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
-
+import FruitsPage from '../FruitsPage/FruitsPage';
+import NavBar from '../../components/NavBar/NavBar';
 export default function App() {
   const [user, setUser] = useState(getUser());
   return (
     <main className={styles.App}>
+      <NavBar />
       { user ?
         <>
           <Routes>
             {/* client-side route that renders the component instance if the path matches the url in the address bar */}
             <Route path="/orders/new" element={<NewOrderPage user={user} setUser={setUser} />} />
             <Route path="/orders" element={<OrderHistoryPage user={user} setUser={setUser} />} />
+            <Route path="/fruits" element={<FruitsPage user={user} setUser={setUser} />} />
             {/* redirect to /orders/new if path in address bar hasn't matched a <Route> above */}
             <Route path="/*" element={<Navigate to="/orders/new" />} />
           </Routes>
