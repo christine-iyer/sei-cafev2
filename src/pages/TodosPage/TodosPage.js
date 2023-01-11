6ef6b721c427a37078ec29b00977535b1bc3fa04
chrisIyer <hellohotbox420@gmail.com>
2023-01-11 10:07:29 -0500

    cleared error messages but need to assign @ users

--- src/pages/TodosPage/TodosPage.js
+++ src/pages/TodosPage/TodosPage.js
@@ -1,105 +1,52 @@
-import { useState, useEffect } from 'react'
-import UserLogOut from '../../components/UserLogOut/UserLogOut';
+
+import { useState, useEffect } from "react"
+import TodoList from "../../components/TodoList/TodoList"
 
 
-export default function ToDosPage({ user, setUser }){
-     const [todos, setTodos] = useState([])
-     const [completedTodos, setCompletedTodos] = useState([])
-     const [newTodo, setNewTodo] = useState({
-         title: '',
-         completed: false
-     })
+
+export default function TodosPage() {
+    const [todos, setTodos] = useState([])
+
+    const addTodo = (e) => {
+        const newTodo = { text: e.target.value, id: Date.now(), completed: false }
+        setTodos([newTodo, ...todos])
+        e.target.value = ''
+    }
+
+
+
+    // const wheresAntony = students.findIndex(student => student.name === 'Antony')
+    // students[wheresAntony].grade = 'A+++'
+    const completeTodo = (id, e) => {
+        const todosCopy = [...todos]
+        const indexOfTodo = todosCopy.findIndex((i) => i.id === id )
+        todosCopy[indexOfTodo].completed = !todosCopy[indexOfTodo].completed
+        setTodos(todosCopy)
+    }
+    const editTodoText = (id, e) => {
+     const todosCopy = [...todos]
+     const indexOfTodo = todosCopy.findIndex((i) => i.id === id)
+     todosCopy[indexOfTodo].text = e.target.value
+     setTodos([...todosCopy])
+     e.target.value = ""
+   }
  
-     //News
-     const createNewTodo = async () => {
-         const body = {...newTodo}
-         try {
-             const response = await fetch('/api/todos', {
-                 method: 'POST',
-                 headers: {
-                     'Content-Type': 'application/json'
-                 },
-                 body: JSON.stringify(body)
-             })
-             const createdTodo = await response.json()
-             const todosCopy = [createdTodo,...todos]
-             setTodos(todosCopy)
-             setNewTodo({
-                 title: '',
-                 completed: false
-             })
-         } catch (error) {   
-             console.error(error)
-         }
-     }
-     //deleteATodos
-     const deleteATodo = async (id) => {
-         try {
-             const index = completedTodos.findIndex((todo) => todo._id === id)
-             const completedTodosCopy = [...completedTodos]
-             const response = await fetch(`/api/todos/${id}`, {
-                 method: 'DELETE',
-                 headers: {
-                     'Content-Type': 'application/json'
-                 }
-             })
-             await response.json()
-             completedTodosCopy.splice(index, 1)
-             setCompletedTodos(completedTodosCopy)
-         } catch (error) {
-             console.error(error)
-         }
-     }
-     //moveToCompleted
-     const moveToCompleted = async (id) => {
-         try {
-             const index = todos.findIndex((todo) => todo._id === id)
-             const todosCopy = [...todos]
-             const subject = todosCopy[index]
-             subject.completed = true 
-             const response = await fetch(`/api/todos/${id}`, {
-                 method: 'PUT',
-                 headers: {
-                     'Content-Type': 'application/json'
-                 },
-                 body: JSON.stringify(subject)
-             })
-             const updatedTodo = await response.json()
-             const completedTDsCopy = [updatedTodo, ...completedTodos]
-             setCompletedTodos(completedTDsCopy)
-             todosCopy.splice(index, 1)
-             setTodos(todosCopy)
-         } catch (error) {
-             console.error(error)
-         }
-     }
-     //getTodos
-     const getTodos = async () => {
-         try{
-             const response = await fetch('/api/todos')
-             const foundTodos = await response.json()
-             setTodos(foundTodos.reverse())
-             const responseTwo = await fetch('/api/todos/completed')
-             const foundCompletedTodos = await responseTwo.json()
-             setCompletedTodos(foundCompletedTodos.reverse())
-         } catch(error){
-             console.error(error)
-         }
-     }
-     useEffect(() => {
-         getTodos()
-     }, [])
-     return(
-         <>
-             
-             newTodo={newTodo}
-             setNewTodo={setNewTodo}
-             createNewTodo={createNewTodo}
-             todos={todos}
-             moveToCompleted={moveToCompleted}
-             completedTodos={completedTodos}
-             deleteATodo={deleteATodo}
-             
-         </>
-     )
- }
+   const deleteTodo = (id) => {
+     const todosCopy = [...todos]
+     const indexOfTodo = todosCopy.findIndex((i) => i.id === id)
+     todosCopy.splice(indexOfTodo, 1)
+     setTodos([...todosCopy])
+   };
+
+  return (
+    <div className="TodoPage">
+      <TodoList
+     todos={todos}
+        addTodo={addTodo}
+        completeTodo={completeTodo}
+        editTodoText={editTodoText}
+        deleteTodo={deleteTodo}
+      />
+    </div>
+  )
+}
