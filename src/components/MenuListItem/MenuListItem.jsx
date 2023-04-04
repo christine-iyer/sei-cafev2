d67c78ffd2d28a5a96cad98596af925f3bd92675
chrisIyer <hellohotbox420@gmail.com>
2023-04-04 19:50:34 -0400

    ok yes greaat. except tlc small

--- src/components/MenuListItem/MenuListItem.jsx
+++ src/components/MenuListItem/MenuListItem.jsx
@@ -3,7 +3,9 @@ import styles from './MenuListItem.module.scss'
 export default function MenuListItem({ menuItem, handleAddToOrder }) {
   return (
     <div className="MenuListItem">
-      <div className={styles.EmojiFlexCtrCtr}>{menuItem.emoji}</div>
+      <div className={styles.EmojiFlexCtrCtr}>
+        <img src={menuItem.emoji}></img>
+        </div>
       <div className={styles.Name}>{menuItem.name}</div>
       <div className="buy">
         <span>${menuItem.price.toFixed(2)}</span>
