a0020b0c6ae6353f594d9b70bf52a127dc13dc26
chrisIyer <hellohotbox420@gmail.com>
2023-04-11 16:07:54 -0400

    tiny change.

--- src/components/Cloudinary/CloudinaryUploadWidget.js
+++ src/components/Cloudinary/CloudinaryUploadWidget.js
@@ -3,24 +3,13 @@ import { Cloudinary } from '@cloudinary/url-gen';
 
 class CloudinaryUploadWidget extends Component {
   componentDidMount() {
-    const cloudName = "dqjhgnivi"; // replace with your own cloud name
-    const uploadPreset = "crystal"; // replace with your own upload preset
+    const cloudName = "dqjhgnivi"; 
+    const uploadPreset = "crystal"; 
 
     var myWidget = window.cloudinary.createUploadWidget(
       {
         cloudName: cloudName,
         uploadPreset: uploadPreset,
-        // cropping: true, //add a cropping step
-        // showAdvancedOptions: true,  //add advanced options (public_id and tag)
-        // sources: [ "local", "url"], // restrict the upload sources to URL and local files
-        // multiple: false,  //restrict upload to a single file
-        // folder: "user_images", //upload files to the specified folder
-        // tags: ["users", "profile"], //add the given tags to the uploaded files
-        // context: {alt: "user_uploaded"}, //add the given context data to the uploaded files
-        // clientAllowedFormats: ["images"], //restrict uploading to image files only
-        // maxImageFileSize: 2000000,  //restrict file size to less than 2MB
-        // maxImageWidth: 2000, //Scales the image down to a width of 2000 pixels before uploading
-        //theme: "purple" ,change to a purple theme
       },
       (error, result) => {
         if (!error && result && result.event === "success") {
@@ -28,6 +17,7 @@ class CloudinaryUploadWidget extends Component {
           document
             .getElementById("uploadedimage")
             .setAttribute("src", result.info.secure_url);
+            console.log( result.info? "result.info.secure_url is "+ result.info.secure_url : "")
         }
       }
     );
@@ -39,11 +29,10 @@ class CloudinaryUploadWidget extends Component {
       false
     );
   }
-
   render() {
     return (
       <button id="upload_widget" className="cloudinary-button">
-        Upload
+       
       </button>
     );
   }
