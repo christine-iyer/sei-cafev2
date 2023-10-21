c2a1b9640753fcc6a9d48c62b6f28ec59cda8c20
chrisIyer <hellohotbox420@gmail.com>
2023-10-21 16:55:26 -0400

    minor.

--- src/components/UserLogOut/UserLogOut.js
+++ src/components/UserLogOut/UserLogOut.js
@@ -1,6 +1,7 @@
+
 import styles from './UserLogOut.module.scss';
 import { logOut } from '../../utilities/users-service';
-import { useState } from 'react'
+
 export default function UserLogOut({ user, setUser }) {
 function handleLogOut() {
   logOut();
